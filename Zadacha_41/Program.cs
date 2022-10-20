3ca33b5c6ce5a058b9beb0a76bd0255561002462
SandyAndy <ansigida77@gmail.com>
2022-10-20 23:43:34 +0300

    Задача 1

---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите через пробел ваш набор чисел:");
+int [] needMassiv = GetArrayFromString (Console.ReadLine());
+
+int result = plusNumbersCount(needMassiv);
+
+Console.WriteLine($"Число положительных членов массива: {result}");
+
+
+//Перевод строкового массива в числовой
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+//Определение колличества положительных членов массива
+int plusNumbersCount (int [] mass)
+{
+    int count = 0;
+    for ( int i = 0; i < mass.Length; i++) if (mass[i]>0) count++;
+    return count;    
+}
